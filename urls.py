f66d63cf2536b7d3958e9e2d91897dc799cc6d6d
user <user@user-MS-7585.(none)>
2012-12-10 16:33:59 +0100

    adding search

--- urls.py
+++ urls.py
@@ -1,14 +1,16 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
+#from django.conf.urls.defaults import *
 from django.contrib import admin
 from doua import settings
 from doua import views
-
+from search.views import search_func
 admin.autodiscover()
 
 urlpatterns = patterns('',
      ('^$', views.main),
      (r'^admin/', include(admin.site.urls)),
-    # (r'^thoughts/', include('thoughts.urls')),
+  #   (r'^search/', include('search.urls')),
+     (r'^search/', search_func, name='search'),
      (r'^static/(?P<path>.*)$', 'django.views.static.serve',
        {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
 
