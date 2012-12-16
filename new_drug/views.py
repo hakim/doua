5bb9ce3b982829728b7717a38c0f2fad1949b4de
user <ahcheriet@gmail.com>
2012-12-16 11:09:19 +0100

    some changing..

--- new_drug/views.py
+++ new_drug/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from app.models import Drogs
+from app.models import Drug
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.models import User
