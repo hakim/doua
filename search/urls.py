7ff7aa49a90a430a222ed160b739a0b4a63d0ea1
user <ahcheriet@gmail.com>
2012-12-23 14:18:15 +0100

    adding a simple search template ..

--- search/urls.py
+++ search/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('search.views',
     # search url
     url(r'^$', search_func, name='search'),
+
 )
--- search/views.py
+++ search/views.py
@@ -5,7 +5,7 @@ from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.views.decorators.csrf  import csrf_exempt, csrf_protect
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
 
 @csrf_exempt
 
@@ -13,7 +13,8 @@ from django.http import HttpResponse
 def search_func(request):
     searched_name = request.POST['query']
     all = Drug.objects.all()
-    if Drug.objects.filter(business_name__iexact=searched_name) or Drug.objects.filter(chemical_name__iexact=searched_name):
-        return HttpResponse("Drug Exist")
+    if Drug.objects.filter(business_name__iexact=searched_name) :
+       d1 =  Drug.objects.get(business_name__iexact=searched_name)
     else:
-        return HttpResponse("Drug doesn\'t Exist")
+       d1 = "Drug  doesn't exist."
+    return render_to_response("result_search.html", {'Searched_drugs':d1},context_instance=RequestContext(request)  )
