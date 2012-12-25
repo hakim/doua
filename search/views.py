c4a947810d90d6902736fcce04a83fb8631789c0
user <ahcheriet@gmail.com>
2012-12-25 14:03:07 +0100

    fixing search ..

--- search/views.py
+++ search/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf  import csrf_exempt, csrf_protect
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
-
+from difflib import *
 
 @csrf_exempt
 def search(request):
@@ -16,11 +16,23 @@ def search(request):
         query = request.GET['query']
         if query != '':
            all_drugs = Drug.objects.all()
+           alter = 0
            for string in query.split():
                drugs = all_drugs.filter(Q(chemical_name__icontains = string)
                                         | Q(business_name__icontains = string)
                                         | Q(description__icontains = string))
+           if not drugs :
+               alter = 2
+               string_l = get_close_matches(query.lower(),[str(d.chemical_name).lower() for d in all_drugs])
+               if string_l:
+                   alter = 1
+               for string in string_l:
+                   drugs = all_drugs.filter(Q(chemical_name__icontains = string)
+                                        | Q(business_name__icontains = string)
+                                        | Q(description__icontains = string))
     context = {
+        'alter':alter,
+        'string':string,
         'drugs' : drugs,
         'request' : request,
         }   
